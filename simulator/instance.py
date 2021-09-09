aa9c60c6d5fd38d4b21d62287dc81955b0739f83
Moj Kazemi <moj.kazemii@gmail.com>
2021-09-10 01:19:54 +0200

    define instance

--- simulator/instance.py
+++ simulator/instance.py
@@ -6,10 +6,10 @@ import numpy as np
 class Instance():
     def __init__(self, sim_setting):
         logging.info("starting simulation...")
-        self.max_size = sim_setting['knapsack_size']
+        # self.max_size = sim_setting['knapsack_size']
 
         # set of supplier: M index by i
-        self.suppliers = sim_setting['num_suplliers']
+        self.suppliers = sim_setting['num_suppliers']
 
         # set of products: K index by k
         self.products = sim_setting['num_products']
@@ -22,40 +22,77 @@ class Instance():
             sim_setting['b_price_low'],
             sim_setting['b_price_high'],
             sim_setting['num_products']
-        ),2)
+        ), 2)
 
-        self.n_items = sim_setting['n_items']
-
-        self.sizes_ss = np.around(np.random.uniform(
-            sim_setting['low_size'],
-            sim_setting['high_size'],
-            sim_setting['n_items']
-        ))
-        self.max_size_ss = sim_setting['knapsack_size']
-
-        self.n_products = np.around(np.random.uniform(
-            sim_setting['low_products'],
-            sim_setting['high_products'],
-            sim_setting['n_products']
+        # set of available quantity for each product k sold by supplier q_ki
+        self.av_quantity = np.around(np.random.uniform(
+            sim_setting['av_quantity_l'],
+            sim_setting['av_quantity_h'],
+            (sim_setting['num_products'], sim_setting['num_suppliers'])
         ))
 
-        logging.info(f"max_size: {self.max_size}")
-        logging.info(f"sizes: {self.sizes}")
-        logging.info(f"profits: {self.profits}")
-        logging.info(f"n_items: {self.n_items}")
-        logging.info(f"sizes_ss: {self.sizes_ss}")
-        logging.info(f"max_size_ss: {self.max_size_ss}")
-        logging.info(f"max_size_ss: {self.n_products}")
-        logging.info("simulation end")
+        # set of price of each product of each supplier f_ki
+        self.product_price = [np.around(np.random.uniform(
+            0.9*self.b_price[k],
+            1.1*self.b_price[k],
+            sim_setting['num_suppliers']
+            ), 2)
+            for k in np.arange(0, sim_setting['num_products'])
+        ]
+
+        # set of penalty price F_K
+        self.penalty_price = [np.around(
+            1.2 * max(self.product_price[k]), 2)
+            for k in np.arange(sim_setting['num_products'])
+        ]
+
+        # set of demand d_k for each product
+        d_had_k = [sim_setting['lambda'] * max(self.av_quantity[k]) + \
+                   (1 - sim_setting['lambda']) * sum(self.av_quantity[k])
+                   for k in np.arange(sim_setting['num_products'])
+                   ]
+
+        self.demand = [np.round(
+            d_had_k[k] - ((d_had_k[k] - 1) * self.b_price[k] / max(self.b_price))
+        )
+            for k in np.arange(sim_setting['num_products'])
+        ]
+
+
+
+
+        # self.n_items = sim_setting['n_items']
+        #
+        # self.sizes_ss = np.around(np.random.uniform(
+        #     sim_setting['low_size'],
+        #     sim_setting['high_size'],
+        #     sim_setting['n_items']
+        # ))
+        # self.max_size_ss = sim_setting['knapsack_size']
+        #
+        # self.n_products = np.around(np.random.uniform(
+        #     sim_setting['low_products'],
+        #     sim_setting['high_products'],
+        #     sim_setting['n_products']
+        # ))
+        #
+        # logging.info(f"max_size: {self.max_size}")
+        # logging.info(f"sizes: {self.sizes}")
+        # logging.info(f"profits: {self.profits}")
+        # logging.info(f"n_items: {self.n_items}")
+        # logging.info(f"sizes_ss: {self.sizes_ss}")
+        # logging.info(f"max_size_ss: {self.max_size_ss}")
+        # logging.info(f"max_size_ss: {self.n_products}")
+        # logging.info("simulation end")
 
     def get_data(self):
         logging.info("getting data from instance...")
         return {
-            "profits": self.profits,
-            "sizes": self.sizes,
-            "max_size": self.max_size,
-            "n_items": self.n_items,
-            "sizes_ss": self.sizes_ss,
-            "max_size_ss": self.max_size_ss,
-            "n_products": self.n_products
+            "suppliers": self.suppliers,
+            "products": self.products,
+            "av_quantity": self.av_quantity,
+            "product_price": self.product_price,
+            "penalty_price": self.penalty_price,
+            "demand": self.demand,
+            # "n_products": self.n_products
         }
--- temptest.py
+++ temptest.py
@@ -1,14 +1,59 @@
 import numpy as np
 import json
+from simulator.instance import Instance
 
 
 fp = open("./etc/sim_setting.json", 'r')
 sim_setting = json.load(fp)
 fp.close()
-a = sim_setting['num_suppliers']
-b = np.around(np.random.uniform(
-            sim_setting['b_price_low'],
-            sim_setting['b_price_high'],
-            sim_setting['num_products']
-       ), 2)
-print(b)
+# a = sim_setting['num_suppliers']
+
+inst = Instance(sim_setting)
+dict_data = inst.get_data()
+
+# print(dict_data['suppliers'])
+
+# b_price = np.around(np.random.uniform(
+#             sim_setting['b_price_low'],
+#             sim_setting['b_price_high'],
+#             sim_setting['num_products']
+#         ), 2)
+#
+# av_quantity = np.around(np.random.uniform(
+#             sim_setting['av_quantity_l'],
+#             sim_setting['av_quantity_h'],
+#             (sim_setting['num_products'], sim_setting['num_suppliers'])
+#         ))
+#
+# product_price = [np.around(np.random.uniform(
+#             0.9*b_price[k],
+#             1.1*b_price[k],
+#             sim_setting['num_suppliers']
+#         ), 2) for k in np.arange(0 , sim_setting['num_products'])]
+#
+# penalty_price = [np.around(
+#     1.2* max(product_price[k]),2)
+#     for k in np.arange(sim_setting['num_products'])]
+#
+# d_had_k = [sim_setting['lambda'] * max(av_quantity[k]) + \
+#            (1 - sim_setting['lambda']) * sum(av_quantity[k])
+#            for k in np.arange(sim_setting['num_products'])
+#            ]
+#
+# demand = [np.round(
+#     d_had_k[k] - ((d_had_k[k] - 1) * b_price[k] / max(b_price))
+#     )
+#     for k in np.arange(sim_setting['num_products'])
+# ]
+
+# print('b_price=\n', b_price)
+# print('av_quantity=\n', av_quantity[0][0])
+# print('product_price=\n',product_price)
+# a=[max(product_price[k]) for k in np.arange(sim_setting['num_products'])]
+# print(a)
+# print('penalty_price=\n', penalty_price)
+# print(b_price)
+# print(b_price[0])
+# print(d_had_k)
+# print('deman = \n', demand)
+
